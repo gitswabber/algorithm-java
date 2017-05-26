ddc0004d897f2d0d78e5aef3b04494b405eb7784
swabber <doutorking@gmail.com>
2017-05-26 16:51:18 +0900

    solving about GetRectangleArea

--- algorithm_java/src/GetRectangleArea.java
+++ algorithm_java/src/GetRectangleArea.java
@@ -3,28 +3,47 @@
     Solving get total area of two rectangles.
 
     CONDITION :
-
     Rectangle1 (K, L)~(M, N)
     Rectangle2 (P, Q)~(R, S)
+    with (lower-left corner, upper-right courner)
 
     K < M
     L < N
     P < R
     Q < S
 
+    The function should return -1 if the area of the sum exceeds 2,147,483,647
+
     -
     SOLVING :
     The condition what is overlapped rectangles
-    1. Sum of the widths of two rectangles > Sum of the widths of two rectangls on the coordinate
-    2. Sum of the heights of two rectangles > Sum of the widths of two rectangls on the coordinate
+    1. Sum of the widths of two rectangles > Range of the widths of two rectangls on the coordinate
+    2. Sum of the heights of two rectangles > Range of the widths of two rectangls on the coordinate
 
  */
 public class GetRectangleArea {
     public int solution(int K, int L, int M, int N, int P, int Q, int R, int S) {
-        int areaOfRectangle1 = (M-K) * (N-L);
-        int areaOfRectangle2 = (Q-P) * (S-R);
+        // long 으로 담아야 하나?
+        int maxArea = 2147483647;
 
-        int sumOfWidths = (M-K) + (Q-P);
-        int sumOfWidthsOnCoordinate = (K < P? K:P) + (M < R? M:R);
+        int areaOfRectangle1 = (M-K) * (N-L);
+        int areaOfRectangle2 = (R-P) * (S-Q);
+        int areaOfOverlappedRectangle = 0;
+
+        int sumOfWidths = (M-K) + (R-P);
+        int sumOfWidthsOnCoordinate = (M > R? M:R) - (K < P? K:P);
+
+        int sumOfHeights = (N-L) + (S-Q);
+        int sumOfHeightsOnCoordinate = (N > S? N:S) - (L < Q? L:Q);
+
+        if((sumOfWidths>sumOfWidthsOnCoordinate) && (sumOfHeights>sumOfHeightsOnCoordinate)) {
+            areaOfOverlappedRectangle = (sumOfWidths-sumOfWidthsOnCoordinate) * (sumOfHeights-sumOfHeightsOnCoordinate);
+        }
+
+        int totalArea = (areaOfRectangle1 + areaOfRectangle2 - areaOfOverlappedRectangle);
+        if(totalArea > maxArea) {
+            return -1;
+        }
+        return totalArea;
     }
 }
--- algorithm_java/src/Main.java
+++ algorithm_java/src/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Solution1 lesson1 = new Solution1();
         System.out.println(lesson1.solution(561892));
+
+        GetRectangleArea getRectangleArea = new GetRectangleArea();
+        System.out.println(getRectangleArea.solution(-4, 1, 2, 6, 0, -1, 4, 3));
     }
 }
